82ebf9557a6280b3eb16c2b2cb1715e5ad34ddc7
Nico <recke.nico@gmail.com>
2025-07-30 19:44:15 -0300

    Mas acciones y antiguedad

--- actualizar_datos.py
+++ actualizar_datos.py
@@ -20,10 +20,13 @@ def get_rsi_status(ticker: str):
     rsi_ultimos = rsi.dropna().iloc[-7:]
     sma_ultimos = sma_rsi.dropna().iloc[-7:]
 
-    cruce_rsi = any(
-        rsi_ultimos.iloc[i - 1] < sma_ultimos.iloc[i - 1] and rsi_ultimos.iloc[i] > sma_ultimos.iloc[i]
-        for i in range(1, len(rsi_ultimos))
-    )
+    cruce_rsi = False
+    dias_desde_cruce_rsi = None
+    for i in range(len(rsi_ultimos)-1, 0, -1):
+        if rsi_ultimos.iloc[i - 1] < sma_ultimos.iloc[i - 1] and rsi_ultimos.iloc[i] > sma_ultimos.iloc[i]:
+            cruce_rsi = True
+            dias_desde_cruce_rsi = len(rsi_ultimos) - i
+            break
 
     # SMAs del precio
     sma_21 = close.rolling(window=21).mean()
@@ -43,11 +46,14 @@ def get_rsi_status(ticker: str):
     macd_line_recent = macd_line.dropna().iloc[-7:]
     signal_line_recent = signal_line.dropna().iloc[-7:]
 
-    cruce_macd = any(
-        macd_line_recent.iloc[i - 1] < signal_line_recent.iloc[i - 1] and
-        macd_line_recent.iloc[i] > signal_line_recent.iloc[i]
-        for i in range(1, len(macd_line_recent))
-    )
+    cruce_macd = False
+    dias_desde_cruce_macd = None
+    for i in range(len(macd_line_recent)-1, 0, -1):
+        if macd_line_recent.iloc[i - 1] < signal_line_recent.iloc[i - 1] and \
+           macd_line_recent.iloc[i] > signal_line_recent.iloc[i]:
+            cruce_macd = True
+            dias_desde_cruce_macd = len(macd_line_recent) - i
+            break
 
     precio_actual = close.iloc[-1]
 
@@ -56,9 +62,11 @@ def get_rsi_status(ticker: str):
         "rsi": round(rsi.iloc[-1], 2),
         "sma_rsi": round(sma_rsi.iloc[-1], 2),
         "rsi_cruce_arriba_sma": cruce_rsi,
+        "dias_desde_cruce_rsi": dias_desde_cruce_rsi,
         "macd": round(macd_line.iloc[-1], 2),
         "macd_signal": round(signal_line.iloc[-1], 2),
         "macd_cruce_arriba_signal": cruce_macd,
+        "dias_desde_cruce_macd": dias_desde_cruce_macd,
         "precio": round(precio_actual, 2),
         "ema_20": round(ema_20.iloc[-1], 2),
         "ema_50": round(ema_50.iloc[-1], 2),
@@ -74,17 +82,24 @@ def get_rsi_status(ticker: str):
         "precio_sma_30": bool(precio_actual > sma_30.iloc[-1])
     }
 
+
 def actualizar():
     # no estan: "AAl",
-    tickers = ["GGAL", "MELI", "EDN", "TSLA", "NVDA", "AMZN", "MMM", "AAL",
-    "AAPL","ACN","ADBE","AMAT","AMD","ANF","ARCO","ASML","AVGO","AXP","C",
-    "CAT","COIN","COST","CRM","CSCO","CVS","DE","DIS","EBAY","EBR","EQNR",
-    "ERIC","ETSY","FDX","FSLR","GLOB","GM","GOOGL","GPRK","GS","GT","HL",
-    "HOG","HSY","IBM","INTC","ITUB","JD","JNJ","JPM","KOF","LLY","LRCX",
-    "MCD","MDLZ","META","MO","MRK","MRNA","MSFT","MSI","NFLX","NIO","NKE",
-    "NOK","NTES","NU","ORCL","PAGS","PBR","PCAR","PEP","PFE","PG","PLTR",
-    "PM","PSX","PYPL","RIO","RIOT","SCCO","SHOP","SLB","SNAP","SPOT","STLA",
-    "TGT","TM","TSM","UNH","VALE","VIST","VRSN","XP","YELP","ZM"]
+    tickers = [
+        'AAL', 'AAPL', 'ABEV', 'ABT', 'ACN', 'ADBE', 'ALUA.BA', 'AMD', 'AMAT', 'AMX', 'AMZN',
+        'ANF', 'ARCO', 'ARKK', 'ASML', 'AVGO', 'AXP', 'AZN', 'BA', 'BABA', 'BAC', 'BB',
+        'BBD', 'BHP', 'BIDU', 'BK', 'BMA', 'BRFS', 'BYMA.BA', 'C', 'CAT', 'CEPU',
+        'COME.BA', 'COIN', 'COST', 'CRM', 'CSCO', 'CVS', 'DE', 'DEO', 'DIA', 'DIS', 'DOCU',
+        'EA', 'EBAY', 'EBR', 'EDN', 'EEM', 'EFX', 'EQNR', 'ERIC', 'ETSY', 'ETHA', 'EWJ',
+        'EWZ', 'FDX', 'FMX', 'FSLR', 'FXI', 'GGAL', 'GLOB', 'GM', 'GOOGL', 'GPRK', 'GRMN',
+        'GS', 'GT', 'HD', 'HL', 'HOG', 'HSY', 'IBM', 'IBIT', 'IEUR', 'INFY', 'INTC', 'IRSA.BA',
+        'ITUB', 'JD', 'JNJ', 'JPM', 'KOF', 'LLY', 'LOMA', 'LRCX', 'MCD', 'MDLZ', 'MELI',
+        'META', 'METR.BA', 'MMM', 'MO', 'MRK', 'MRNA', 'MSFT', 'MSI', 'NFLX', 'NIO', 'NKE',
+        'NOK', 'NTES', 'NU', 'NVDA', 'ORCL', 'ORLY', 'PAAS', 'PAGS', 'PAMP.BA', 'PBR', 'PCAR',
+        'PEP', 'PFE', 'PG', 'PLTR', 'PM', 'PSX', 'PYPL', 'QQQ', 'RIO', 'RIOT', 'ROKU', 'SBUX',
+        'SCCO', 'SHOP', 'SLB', 'SNAP', 'SPOT', 'SPY', 'STLA', 'TGSU2.BA', 'TGT', 'TGNO4.BA', 'TM',
+        'TRAN.BA', 'TSLA', 'TSM', 'TXAR.BA', 'UNH', 'VALE', 'VALO.BA', 'VIST', 'VRSN', 'XP', 'XLV',
+        'XLC', 'XLP', 'XLY', 'YELP', 'YPFD.BA', 'ZM']
 
     resultados = [get_rsi_status(t) for t in tickers]
     with open("data/indicadores.json", "w") as f:
